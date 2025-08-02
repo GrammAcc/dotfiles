5b765e1a0e6d94531aa035808e193d1e3c475f7d
Dalton Lang <35282898+GrammAcc@users.noreply.github.com>
2025-08-02 11:27:22 -0500

    Fix typscript lsp config

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local telescope = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>fk", telescope.git_files)
-vim.keymap.set("n", "<leader>fj", telescope.find_files)
-vim.keymap.set("n", "<leader>fl", telescope.live_grep)
+vim.keymap.set("n", "<leader>fj", telescope.live_grep)
+vim.keymap.set("n", "<leader>fl", telescope.find_files)
 vim.keymap.set("n", "<leader>f;", telescope.grep_string)
 vim.keymap.set("n", "<leader>fo", ":Telescope<CR>")

a31dc107bfdb875a88bf4b9c7d7aa658854fdb3e
Dalton Lang <35282898+GrammAcc@users.noreply.github.com>
2025-08-08 19:03:01 -0500

    Add helper func for updating neovim

--- zshconfig/funcs/core.zsh
+++ zshconfig/funcs/core.zsh
@@ -1,14 +1,12 @@
-# Perform a diff between two JSON files. ${1} is original, ${2} is changed.
-jsondiff () {
-	diff <(cat ${1} | jq) <(cat ${2} | jq)
+neovimupdate () {
+    currdir=$PWD
+    cd ~/build/neovim/
+    git checkout master
+    git fetch
+    git checkout stable
+    make distclean
+    mv ~/neovim ~/neovim$(date +%s).bak
+    make CMAKE_BUILD_TYPE=$1 CMAKE_INSTALL_PREFIX=~/neovim install
+    cd $currdir
 }
 
-# Find and replace all occurences of string ${1} with string ${2} in all files with extension ${3} recursively from the current directory
-findreplace () {
-    sed -i -- "s/${1}/${2}/g" **/*${3}(.^D)
-}
-
-# Start podman container with specified compose file.
-podtestup () {
-    podman-compose -f ${1} up --build --force-recreate --remove-orphans --no-cache --detach
-}
